2f247be323d69807b0652a28af44086f584b157a
Kiran Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2026-06-18 15:46:16 +0530

    Settings

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import streamlit as st
 load_dotenv()
 client  = Groq(api_key=os.getenv("GROQ_API_KEY"))
 st.header("Groq Bot")
+st.subheader("Settings")
 if "messages" not in st.session_state:
     st.session_state.messages = [{
         "role" : "system",
